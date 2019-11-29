a28e5b0cd9037e3ad9dab090671a1ec521d20c0c
Nate Hodge <nhodge253@gmail.com>
2019-11-29 00:24:27 -0800

    Components part 1

---
+++ components/MenuComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+function Menu(props) {
+
+    const renderMenuItem = ({item, index}) => {
+
+        return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: require('./images/uthappizza.png')}}
+                  />
+        );
+    };
+
+    return (
+            <FlatList 
+                data={props.dishes}
+                renderItem={renderMenuItem}
+                keyExtractor={item => item.id.toString()}
+                />
+    );
+}
+
+export default Menu;
