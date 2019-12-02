b20c818bceb65fcdb990bae4d99fb7581b81012f
Nate Hodge <nhodge253@gmail.com>
2019-12-01 22:09:00 -0800

    added buttons and icons to drawer menu as well as imported comments into dish detail

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -14,15 +14,11 @@ class Contact extends Component {
           featuredTitle="Reach Out"
           image={require("./images/interior.jpg")}
         >
-          <Text style={{ margin: 10 }}>
-            {`
-          121, Clear Water Bay Road~
-          Clear Water Bay, Kowloon HONG KONG~
-          Tel:+852 1234 5678~
-          Fax: +852 8765 4321~
-          Email:confusion@food.net
-         `}
-          </Text>
+          <Text style={{ margin: 10 }}>121, Clear Water Bay Road</Text>
+          <Text style={{ margin: 10 }}>Clear Water Bay, Kowloon HONG KONG</Text>
+          <Text style={{ margin: 10 }}>Tel:+852 1234 5678</Text>
+          <Text style={{ margin: 10 }}>Fax: +852 8765 4321</Text>
+          <Text style={{ margin: 10 }}>Email:confusion@food.net</Text>
         </Card>
       </ScrollView>
     );
