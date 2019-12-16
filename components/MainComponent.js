1db02f0003b0384b6a3c42578f1fac9a213161c1
Nate Hodge <nhodge253@gmail.com>
2019-12-15 16:07:01 -0800

    added login component with secure store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Reservation from "./ReservationComponent";
 import Favorites from "./FavoriteComponent";
+import Login from "./LoginComponent";
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from "react-native";
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from "react-navigation";
 import { Icon } from "react-native-elements";
@@ -150,6 +151,24 @@ const FavoritesNavigator = createStackNavigator(
   }
 );
 
+const LoginNavigator = createStackNavigator(
+  {
+    Login: { screen: Login }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerLeft: <Icon name="menu" size={24} color="white" onPress={() => navigation.toggleDrawer()} />
+    })
+  }
+);
+
 const CustomDrawerContentComponent = props => (
   <ScrollView>
     <SafeAreaView style={StyleSheet.container} forceInset={{ top: "always", horizonatal: "never" }}>
@@ -168,6 +187,14 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
   {
+    Login: {
+      screen: LoginNavigator,
+      navigationOptions: {
+        title: "Login",
+        drawerLabel: "Login",
+        drawerIcon: ({ tintColor, focused }) => <Icon name="sign-in" type="font-awesome" size={24} color={tintColor} />
+      }
+    },
     Home: {
       screen: HomeNavigator,
       navigationOptions: {
@@ -221,6 +248,7 @@ const MainNavigator = createDrawerNavigator(
     }
   },
   {
+    initialRouteName: "Home",
     drawerBackgroundColor: "#D1C4E9",
     contentComponent: CustomDrawerContentComponent
   }
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://192.168.1.81:3001/";
+export const baseUrl = "http://10.0.0.211:3001/";
