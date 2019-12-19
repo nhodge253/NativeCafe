33ce09f7b1394c23ba047fc50d83eb140659549d
Nate Hodge <nhodge253@gmail.com>
2019-12-18 16:18:25 -0800

    added Toast connection type notification and image manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import { createBottomTabNavigator } from "react-navigation";
 import { baseUrl } from "../shared/baseUrl";
+import * as ImageManipulator from "expo-image-manipulator";
 
 class LoginTab extends Component {
   constructor(props) {
@@ -122,9 +123,17 @@ class RegisterTab extends Component {
       });
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
-        this.setState({ imageUrl: capturedImage.uri });
+        this.processImage(capturedImage.uri);
       }
     }
+  };
+
+  processImage = async imageUri => {
+    let processedImage = await ImageManipulator.manipulateAsync(imageUri, [{ resize: { width: 400 } }], {
+      format: "png"
+    });
+    console.log(processedImage);
+    this.setState({ imageUrl: processedImage.uri });
   };
 
   static navigationOptions = {
